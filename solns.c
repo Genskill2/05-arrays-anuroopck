8dd7aacd86ee2df8ede6bf5fce3e2093e6712cbf
anuroopck <79464093+anuroopck@users.noreply.github.com>
2021-06-13 19:15:03 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,14 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+int max (int ar[], int numbr)
+{   int highest=0;
+    for (int i=0; i<numbr; i++ )
+    {
+        if (highest < ar[i])
+        {
+          highest = ar[i];
+        }
+    }
+    return highest;
+}
