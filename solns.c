773e6ea5f981a84e9dc8fc189a1a9abd2ac52206
anuroopck <79464093+anuroopck@users.noreply.github.com>
2021-06-13 20:45:44 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -25,9 +25,26 @@ int min (int ar[], int numbr)
 }
 float average (int ar[], int numbr)
 {   float average = 0;
-    for (int i=1; i<numbr; i++ )
+    for (int i=0; i<numbr; i++ )
     {
         average = average + ar[i];
     }
     return (average/numbr);
+}
+int mode (int ar[], int numbr)
+{   int count, highest = 0;
+    for (int i=0; i<numbr; i++ )
+    {   count = 1;
+        for (int j = 0; j < i; j++)
+        {
+          if(ar[j] == ar[i] )
+          {
+            count = count + 1;
+          }
+          if (count > highest) {
+            highest = count;
+          }
+        }
+    }
+    return highest;
 }
